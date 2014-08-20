ca9f02e73b74cac229bbd7c4fa8bab44e4c4a8fd
Tom Hanley <tjhanleyx@gmail.com>
2014-08-20 16:02:32 -0700

    clean up copy and pasted methods

--- spec/fifthgear/export_item_personalization_data_spec.rb
+++ spec/fifthgear/export_item_personalization_data_spec.rb
@@ -5,7 +5,7 @@ module Fifthgear
     describe "#get" do
       it "should return an item" do
         pending "Configuration isn't passing username and password to client."
-        item = Fifthgear.('testsku')
+        item = Fifthgear.export_item_personalization_data('testsku')
         expect(item).to_not eq(nil)
       end
     end
--- spec/fifthgear/item_inventory_lookup_spec.rb
+++ spec/fifthgear/item_inventory_lookup_spec.rb
@@ -5,7 +5,7 @@ module Fifthgear
     describe "#get" do
       it "should return details about item" do
         pending "Configuration isn't passing username and password to client."
-        item = Fifthgear.item__inventory_lookup('testsku')
+        item = Fifthgear.item_inventory_lookup('testsku')
         expect(item).to_not eq(nil)
       end
     end
