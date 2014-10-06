811712b4cfd61fec0af1c58dfb42158e8f959006
Tom Hanley <tjhanleyx@gmail.com>
2014-10-06 15:31:16 -0700

    Force date to_s

--- lib/fifthgear/helper.rb
+++ lib/fifthgear/helper.rb
@@ -2,7 +2,7 @@ require 'chronic'
 module Fifthgear
   module Helper
     def self.format_date(date=nil)
-      date = Chronic.parse(date)
+      date = Chronic.parse(date.to_s)
       sprintf('\/Date(%s%s)\/', date.strftime('%s').to_i*1000, date.strftime('%z'))
     end
   end
