94c5d1966ba43a806a289f4a0934ceb38411d715
Thomas Hanley <tjhanleyx@gmail.com>
2014-10-08 19:07:07 -0700

    Remove debug block

--- lib/fifthgear/cart.rb
+++ lib/fifthgear/cart.rb
@@ -1,12 +1,6 @@
 module Fifthgear
   class Cart
     def self.submit(cart={})
-      if Fifthgear.configuration.debug
-        require 'json'
-        File.open("/tmp/output-#{Time.now.to_i}.json","w") do |f|
-          f.write({ "CompanyId" => Fifthgear.configuration.company_id, "Request" => cart}.to_json)
-        end
-      end
       conn = Fifthgear::Client.new
       conn.post "#{Fifthgear.configuration.api_version}/CartSubmit", { "CompanyId" => Fifthgear.configuration.company_id, "Request" => cart}
     end
