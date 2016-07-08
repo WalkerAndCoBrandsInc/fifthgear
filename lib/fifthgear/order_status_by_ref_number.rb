ec472727a0f36f7aac88c12d6e06393f8aef0b85
Rachel Heaton and Senthil Arivudainambi <pair+rachel@getbevel.com+senthil@getbevel.com@>
2016-07-08 15:51:53 -0700

    Fix failing tests, update api

--- lib/fifthgear/order_status_by_ref_number.rb
+++ lib/fifthgear/order_status_by_ref_number.rb
@@ -1,8 +1,13 @@
 module Fifthgear
   class OrderStatusByRefNumber
-    def self.get(orderRefNumber=nil)
+    def self.lookup(orderRefNumber=nil)
       conn = Fifthgear::Client.new
       conn.post "#{Fifthgear.configuration.api_version}/OrderStatusLookupByRefNumber", { "CompanyId" => Fifthgear.configuration.company_id, "Request" => orderRefNumber}
+    end
+
+    def self.get(orderRefNumber=nil)
+      warn "Fifthgear::OrderStatusByRefNumber.get is deprecated, use .lookup"
+      lookup(orderRefNumber)
     end
   end
 end
